cb702ace86ae59715fd92d380312423dbd3d6411
Kierancz <kieran.czerwinski@colorado.edu>
2014-03-14 23:57:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalpApp::Application.config.secret_key_base = 'a433abc767992d054448677df25e3b79ae7316f3ccad9e9953c45821fdae9d3e83eff454a9bb6dc68d27b5c5d070dd030359b4b81b23c9360773569c46670807'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinalpApp::Application.config.session_store :cookie_store, key: '_finalp_app_session'
