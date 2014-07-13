a38f8b9da6321bea9c0bb130e38013900da521f7
Kierancz <kieran.czerwinski@colorado.edu>
2014-07-12 19:09:56 -0600

    updated mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,11 @@ BlockSpace::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    tls: true,
+    enable_starttls_auto: true, 
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain", 
-    enable_starttls_auto: true, 
     user_name: ENV["GMAIL_USERNAME"], 
     password: ENV["GMAIL_PASSWORD"]
   }
