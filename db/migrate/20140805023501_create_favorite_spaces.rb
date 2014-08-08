407b09ab3ef950572e6d1fa25b9f275ac9ecc0da
Kierancz <kieran.czerwinski@colorado.edu>
2014-08-08 00:23:55 -0600

    added new files

---
+++ db/migrate/20140805023501_create_favorite_spaces.rb
@@ -0,0 +1,11 @@
+class CreateFavoriteSpace < ActiveRecord::Migration
+  def change
+    create_table :favorite_spaces do |t|
+      t.string :space_id
+      t.string :integer
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
