f2e6ea080851b8b0a9d0ea19ad2f587f32b2e06f
Kierancz <kieran.czerwinski@colorado.edu>
2014-08-08 00:59:23 -0600

    fix for heroku

--- db/migrate/20140808064554_fix_favorites_table.rb
+++ db/migrate/20140808064554_fix_favorites_table.rb
@@ -1,6 +1,6 @@
 class FixFavoritesTable < ActiveRecord::Migration
   def change
-  	change_column :favorite_spaces, :space_id, :integer
+  	change_column :favorite_spaces, :space_id, 'integer USING CAST(column_name AS integer)'
   	remove_column :favorite_spaces, :integer
   end
 end
