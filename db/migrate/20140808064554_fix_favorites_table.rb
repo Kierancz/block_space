b44b9930bff1efc73eed5db5375ba848688eb2aa
Kierancz <kieran.czerwinski@colorado.edu>
2014-08-08 01:03:24 -0600

    small fix

--- db/migrate/20140808064554_fix_favorites_table.rb
+++ db/migrate/20140808064554_fix_favorites_table.rb
@@ -1,6 +1,6 @@
 class FixFavoritesTable < ActiveRecord::Migration
   def change
-  	change_column :favorite_spaces, :space_id, 'integer USING CAST(:favorite_spaces AS integer)'
+  	change_column :favorite_spaces, :space_id, 'integer USING CAST(favorite_spaces AS integer)'
   	remove_column :favorite_spaces, :integer
   end
 end
