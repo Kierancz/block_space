2df7d3eab01298905c6c4e923252c76ec0db3276
Kierancz <kieran.czerwinski@colorado.edu>
2014-08-17 16:29:57 -0600

    attempted bug fix

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,5 +5,5 @@ class Space < ActiveRecord::Base
 	has_many :favorited_by, through: :favorite_spaces, source: :user
 
 	scope :desc, order("spaces.updated_at DESC")
-	scope :fav, Space.joins(:favorite_spaces).group("favorite_spaces.space_id").order("count(favorite_spaces.space_id) ASC")
+	scope :fav, Space.joins(:favorite_spaces).select('spaces.*, count(space_id) as "favorite_space_count"').group(:space_id).order("favorite_space_count ASC")
 end
