57e22fcf713b4a5374f6981e8842a433bdfe4b75
Kierancz <kieran.czerwinski@colorado.edu>
2014-08-17 17:53:50 -0600

    work on favorite space scope

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,5 +5,10 @@ class Space < ActiveRecord::Base
 	has_many :favorited_by, through: :favorite_spaces, source: :user
 
 	scope :desc, order("spaces.updated_at DESC")
-	scope :fav, Space.joins(:favorite_spaces).select('spaces.*, count(space_id) as "favorite_space_count"').group(:space_id).order("favorite_space_count ASC")
+	#scope :fav, Space.joins(:favorite_spaces).select('spaces.*, count(space_id) as "favorite_space_count"').group(:space_id).order("favorite_space_count ASC")
+	scope :fav,
+		select("spaces.*, count(favorite_spaces.id) AS favorite_space_count").
+		joins(:favorite_spaces).
+		group("spaces.id").
+		order("favorite_space_count DESC")
 end
